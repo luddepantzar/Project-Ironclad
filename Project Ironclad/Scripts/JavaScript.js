a91285ebca71d9082a555ef0343557d8314d7775
lubbejareb <ludwigjareb@gmail.com>
2018-04-12 14:47:05 +0200

    Clock JS

---
+++ Project Ironclad/Scripts/JavaScript.js
@@ -0,0 +1,18 @@
+﻿{
+    function RealTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+
+    m = checkTime(m);
+    document.getElementById('txt').innerHTML =
+    h + ":" + m;
+        var t = setTimeout(RealTime, 500);
+}
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i}; 
+        return i;
+}
+
+}
