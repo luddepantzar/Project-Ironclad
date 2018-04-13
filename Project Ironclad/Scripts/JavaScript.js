d73bed74db939da2e3d1bbc1d035420d3fd96568
lubbejareb <ludwigjareb@gmail.com>
2018-04-13 08:20:42 +0200

    Flipcode

--- Project Ironclad/Scripts/JavaScript.js
+++ Project Ironclad/Scripts/JavaScript.js
@@ -1,18 +1,16 @@
 ﻿{
     function RealTime() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
+        var today = new Date();
+        var h = today.getHours();
+        var m = today.getMinutes();
 
-    m = checkTime(m);
-    document.getElementById('txt').innerHTML =
-    h + ":" + m;
+        m = checkTime(m);
+        document.getElementById('txt').innerHTML =
+            h + ":" + m;
         var t = setTimeout(RealTime, 500);
-}
-
-function checkTime(i) {
-    if (i < 10) {i = "0" + i}; 
+    }
+    function checkTime(i) {
+        if (i < 10) { i = "0" + i };
         return i;
-}
-
+    }
 }
